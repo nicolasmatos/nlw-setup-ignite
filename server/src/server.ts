d59e59415c5c0b590192b82cc8c9e76e15b81794
Nicolas Matos <nicolasmatos0905@gmail.com>
2023-01-17 20:45:21 -0300

    Aula 1 - Setup Backend (Node.Js)

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import Fastify from "fastify"
+import cors from "@fastify/cors"
+import { PrismaClient } from "@prisma/client"
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get("/habits", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "Ha"
+      }
+    }
+  })
+
+  return habits
+})
+
+app.listen({
+  port: 3333,
+  host: "0.0.0.0",
+}) .then(() => {
+  console.log("Server running at http://localhost:3333")
+})
