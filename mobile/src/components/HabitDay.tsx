eba3f5b3d192c0d16677d43411f762feca4fbbbb
Nicolas Matos <nicolasmatos0905@gmail.com>
2023-01-20 23:34:18 -0300

    Aula 4 - Conexão com API para cadastro de novo hábito e população do sumário (React Native)

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,8 @@
 import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import clsx from "clsx";
+
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
+import dayjs from "dayjs";
 
 const weekDays = 7
 const screenHorizontalPadding = (32 * 2) / 5
@@ -6,15 +10,33 @@ const screenHorizontalPadding = (32 * 2) / 5
 export const dayMarginBetween = 8
 export const daySize = (Dimensions.get('screen').width / weekDays) - (screenHorizontalPadding + 5)
 
-interface Props extends TouchableOpacityProps {}
+interface Props extends TouchableOpacityProps {
+  amount?: number;
+  completed?: number;
+  date: Date;
+}
 
-export function HabitDay({ ...rest }: Props) {
+export function HabitDay({ amount = 0, completed = 0, date, ...rest }: Props) {
+  const amountAccomplishedPercentage = amount > 0 ? generateProgressPercentage(amount, completed) : 0
+  const today = dayjs().startOf('day').toDate();
+  const isCurrentDay = dayjs(date).isSame(today);
+
   return (
     <TouchableOpacity
-        className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
-        style={{ width: daySize, height: daySize }}
-        activeOpacity={0.7}
-        {...rest }
+      className={clsx(
+        "rounded-lg border-2 m-1", {
+          ["bg-zinc-900 border-zinc-800"] : amountAccomplishedPercentage === 0,
+          ["bg-violet-900 border-violet-700"] : amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 20,
+          ["bg-violet-800 border-violet-600"] : amountAccomplishedPercentage > 20 && amountAccomplishedPercentage < 40,
+          ["bg-violet-700 border-violet-500"] : amountAccomplishedPercentage > 40 && amountAccomplishedPercentage < 60,
+          ["bg-violet-600 border-violet-500"] : amountAccomplishedPercentage > 60 && amountAccomplishedPercentage < 80,
+          ["bg-violet-500 border-violet-400"] : amountAccomplishedPercentage > 80,
+          ["border-white border-4"] : isCurrentDay,
+        }
+      )}
+      style={{ width: daySize, height: daySize }}
+      activeOpacity={0.7}
+      {...rest }
     />
   )
 }
--- mobile/src/components/SummaryTable.tsx
+++ mobile/src/components/SummaryTable.tsx
@@ -1,17 +1,55 @@
-import { View, Text, ScrollView } from "react-native";
+import { useEffect, useState } from 'react';
+import { View, Text, ScrollView, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import { api } from '../lib/axios';
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates'
 
 import { HabitDay, daySize } from "../components/HabitDay";
+import { Loading } from './Loading';
+
+import dayjs from 'dayjs';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const summaryDates = generateRangeDatesFromYearStart()
 const minimumSummaryDatesSize = 18 * 5 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
+type SummaryProps = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}>
+
 export function SummaryTable() {
+  const [loading, setLoading] = useState(true)
+  const [summary, setSummary] = useState<SummaryProps | null>(null)
+
   const { navigate } = useNavigation()
+
+  async function fetchData() {
+    try {
+      setLoading(true)
+      const response = await api.get('/summary');
+      setSummary(response.data)
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos.')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <ScrollView
@@ -31,25 +69,38 @@ export function SummaryTable() {
           ))
         }
       </View>
-      <View className="flex-row flex-wrap">
-        {
-          summaryDates.map(date => (
-            <HabitDay
-              key={date.toISOString()}
-              onPress={() => navigate('habit', { date: date.toISOString() })}
-            />
-          ))
-        }
-        {
-        amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => (
-          <View
-            key={i}
-            className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-            style={{ width: daySize, height: daySize }}
-          />
-        ))
+      {
+        summary && (
+          <View className="flex-row flex-wrap">
+            {
+              summaryDates.map(date => {
+                const dayWithHabits = summary.find(day => {
+                  return dayjs(date).isSame(day.date, 'day')
+                })
+
+                return (
+                  <HabitDay
+                    key={date.toISOString()}
+                    date={date}
+                    amount={dayWithHabits?.amount}
+                    completed={dayWithHabits?.completed}
+                    onPress={() => navigate('habit', { date: date.toISOString() })}
+                  />
+                )
+              })
+            }
+            {
+              amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+                <View
+                  key={i}
+                  className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                  style={{ width: daySize, height: daySize }}
+                />
+              ))
+            }
+          </View>
+        )
       }
-      </View>
     </ScrollView>
   )
 }
