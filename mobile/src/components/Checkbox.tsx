2e65cc04b79c3330e5afb416a896f43e4e9bcc90
Nicolas Matos <nicolasmatos0905@gmail.com>
2023-01-22 21:56:38 -0300

    Aula 5 - Finalização da versão mobile da aplicação (React Native)

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,6 +1,7 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native"
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import { Feather } from '@expo/vector-icons'
-import colors from "tailwindcss/colors"
+import colors from "tailwindcss/colors";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 
 interface Props extends TouchableOpacityProps {
   title: string;
@@ -14,16 +15,20 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       className="flex-row mb-2 items-center"
       {...rest}
     >
-      { checked
+      { checked 
         ?
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-          <Feather
+        <Animated.View 
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
+          <Feather 
             name="check"
             size={20}
             color={colors.white}
           />
-        </View>
-        :
+        </Animated.View>
+        : 
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       }
 
