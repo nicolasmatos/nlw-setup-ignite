659de8da993bee5c73f43e87fa5293fe88d5fa7f
Nicolas Matos <nicolasmatos0905@gmail.com>
2023-06-20 12:11:12 -0300

    Add

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,9 +3,20 @@ import './lib/dayjs'
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 
-// import { Habit } from "./components/Habit"
+navigator.serviceWorker.register('service-worker.js')
 
 export function App() {
+  /*
+  window.Notification.requestPermission(permission => {
+    if (permission === 'granted') {
+      console.log('Permission granted')
+      new window.Notification('Habits', {
+        body: 'Texto',
+      })
+    }
+  })
+  */
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
